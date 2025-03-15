a069cff1e6462b495e26f0be7b5dca1fc8e1d0e9
Stefani Kostic <stefanikostic6@gmail.com>
2025-03-15 17:25:46 +0100

    Added error message for pattern of username

--- src/main/java/com/evelateresume/user_service/dto/AuthRequest.java
+++ src/main/java/com/evelateresume/user_service/dto/AuthRequest.java
@@ -4,6 +4,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record AuthRequest(@NotNull
-                          @Pattern(regexp = "^[A-Za-z]+(?: [A-Za-z]+)*$", message = "Full name can only contain alphabetic characters and spaces.")
+                          @Pattern(regexp = "^[a-zA-Z0-9_]{3,20}$",
+                                  message = "Username must be between 3 and 20 characters and can only contain " +
+                                          "letters, numbers, and underscores.")
                           String username,
                           @NotNull String password) {}
