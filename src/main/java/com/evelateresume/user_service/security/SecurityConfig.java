d97b3a4cf0bf1da28b2c1abf8e4641419a5304d6
Stefani Kostic <stefanikostic6@gmail.com>
2025-03-15 17:27:24 +0100

    Added /error to public routes.

--- src/main/java/com/evelateresume/user_service/security/SecurityConfig.java
+++ src/main/java/com/evelateresume/user_service/security/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     private static final String LOGIN_PATH = "/auth/login";
     private static final String REGISTER_PATH = "/auth/register";
 
-    private static final String[] PUBLIC_ROUTES = { LOGIN_PATH, REGISTER_PATH };
+    private static final String[] PUBLIC_ROUTES = { LOGIN_PATH, REGISTER_PATH, "/error" };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
