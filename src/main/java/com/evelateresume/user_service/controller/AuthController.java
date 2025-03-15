99379bbbc56fd1b9b2ecea25b155521982de78f2
Stefani Kostic <stefanikostic6@gmail.com>
2025-03-15 17:27:07 +0100

    Added cookie with token in response.

--- src/main/java/com/evelateresume/user_service/controller/AuthController.java
+++ src/main/java/com/evelateresume/user_service/controller/AuthController.java
@@ -3,8 +3,10 @@ package com.evelateresume.user_service.controller;
 import com.evelateresume.user_service.dto.AuthRequest;
 import com.evelateresume.user_service.dto.RegisterRequest;
 import com.evelateresume.user_service.service.AuthenticationService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,19 +18,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    public static final String TOKEN_COOKIE_NAME = "token";
     private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody AuthRequest authRequest) {
+    public ResponseEntity<String> login(@RequestBody @Valid AuthRequest authRequest, HttpServletResponse response) {
         String token = authenticationService.loginUser(authRequest);
-        if (token != null) {
-            return ResponseEntity.ok(token);
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+
+        Cookie cookie = new Cookie(TOKEN_COOKIE_NAME, token);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false); // Change to true in production (for HTTPS)
+        cookie.setPath("/");
+        cookie.setMaxAge(60 * 60);
+        response.addCookie(cookie);
+        return ResponseEntity.ok(token);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
+    public ResponseEntity<String> register(@RequestBody @Valid RegisterRequest registerRequest) {
         authenticationService.registerUser(registerRequest);
 
         return ResponseEntity.ok("Successfully registered user.");
